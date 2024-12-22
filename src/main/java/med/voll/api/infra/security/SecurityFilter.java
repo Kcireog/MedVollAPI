13bc46db2cb4d2b2881d55438077ee9ba85ee3c4
Erick Rodriguez <rodriguezsantiago0230@hotmail.com>
2024-12-22 01:15:25 -0500

    Updating the SecurityFilter class, to display the subject of the user's login request as a part of a development test

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,13 +4,18 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
+@Component //desactivar la etiqueta, para generar un nuevo toquen sin la rstricción de expiración que asignamos (2hrs)
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
@@ -19,8 +24,11 @@ public class SecurityFilter extends OncePerRequestFilter {
         if(token==null || token.isEmpty()){
             throw new RuntimeException("El token enviado no es valido...");
         }
-        token= token.replace("Bearer", "");
+        token= token.replace("Bearer ", "");
         System.out.println(token);
+
+        //verificar que el subject(login) es un user logueado en el sistema
+        System.out.println(tokenService.getSubject(token)); // Este usuario tiene sesion?
 
         //utilizar la cadena de filtro, para pasar al sguiente filtro el handling de la request
         filterChain.doFilter(request, response);//que el fltro llame al siguiente, para que llegue al controller
