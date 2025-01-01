3cb1f7adfa4d7a8c198ef6d52bea175ff22fe11f
Erick Rodriguez <rodriguezsantiago0230@hotmail.com>
2025-01-01 17:46:51 -0500

    Adding ValidacionException class

--- src/main/java/med/voll/api/domain/consulta/DatosReservaConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DatosReservaConsulta.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public record DatosReservaConsulta(
-        Long idMedico, //puede ser null, porque podemos seleccionar un medico random
+        Long idMedico, //puede ser null, porque se asignara un medico random, en caso de que el id no exista
         @NotNull
         Long idPaciente,
         @NotNull
--- src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
@@ -1,7 +1,8 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.ValidacionException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
-import med.voll.api.domain.pacientes.Paciente;
 import med.voll.api.domain.pacientes.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,11 +19,21 @@ public class ReservaDeConsultas {
 
     public void reservar(DatosReservaConsulta datos) {
 
-        var medico = medicoRepository.findById(datos.idMedico()).get();//el get para evitar obtener un Optional
-        var paciente = pacienteRepository.findById(datos.idPaciente()).get();
-
+        if(!pacienteRepository.existsById(datos.idPaciente())){
+            throw new ValidacionException("No existe un paciente con el id informado...");
+        }
+        if(datos.idMedico()!= null && !medicoRepository.existsById(datos.idMedico())){
+            throw new ValidacionException("No existe un medico con el id informado...");
+        }
+        var medico = elegirMedico(datos);
+        var paciente = pacienteRepository.findById(datos.idPaciente()).get();//el get para evitar obtener un Optional
         var consulta = new Consulta(null, medico, paciente, datos.fecha());
 
         consultaRepository.save(consulta);
+    }
+
+    private Medico elegirMedico(DatosReservaConsulta datos) {
+
+
     }
 }
