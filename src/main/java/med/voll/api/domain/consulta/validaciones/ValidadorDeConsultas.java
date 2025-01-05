6b55a2ac0e8b8cdf5d7adc07206bd3d85fdb1d91
Erick Rodriguez <rodriguezsantiago0230@hotmail.com>
2025-01-05 17:07:36 -0500

    Add 'ValidadorDeConsultas' interface

---
+++ src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorDeConsultas.java
@@ -0,0 +1,7 @@
+package med.voll.api.domain.consulta.validaciones;
+
+import med.voll.api.domain.consulta.DatosReservaConsulta;
+
+public interface ValidadorDeConsultas {
+    void validar(DatosReservaConsulta datos);// todos los metodos de una interfaz son publicos
+}
