d280e36bfa52f96eb00910e2796768a64f2cb553
Erick Rodriguez <rodriguezsantiago0230@hotmail.com>
2025-01-06 02:04:50 -0500

    Fix parameter order in 'Duration.between()'

--- src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorConsultaConAnticipacion.java
+++ src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorConsultaConAnticipacion.java
@@ -8,14 +8,14 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Component //componenete generico que necesitamos que Spring cargue automaticamente
-public class ValidadorConsultaConAnticipacion implements ValidadorDeConsultas{
+public class ValidadorConsultaConAnticipacion implements ValidadorDeConsultas {
 
-    public void validar(DatosReservaConsulta datos){
+    public void validar(DatosReservaConsulta datos) {
         var fechaConsulta = datos.fecha();
         var ahora = LocalDateTime.now();//horario de ahora
-        var diferenciaMinutos = Duration.between(fechaConsulta, ahora).toMinutes();
+        var diferenciaMinutos = Duration.between(ahora, fechaConsulta).toMinutes();
 
-        if(diferenciaMinutos < 30){
+        if (diferenciaMinutos < 30) {
             throw new ValidacionException("Horario seleccionado menor que 30min de anticipación");
         }
 
