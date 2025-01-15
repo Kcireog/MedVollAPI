3f4a59e13aedeb45ee8d13c90067d8b4ff8f0be2
Erick Rodriguez <rodriguezsantiago0230@hotmail.com>
2025-01-15 00:50:35 -0500

    Add 'ConsultaControllerTest'

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -1,10 +1,11 @@
 package med.voll.api.domain.consulta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 
+@Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
 
